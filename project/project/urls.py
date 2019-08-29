cd5dcb98fb453458aab9a22c7f15021b1589cc25
samuel.gondouin <samuel.gondouin@gandi.net>
2019-08-29 08:30:33 +0200

    Update urls config

--- project/project/urls.py
+++ project/project/urls.py
@@ -2,9 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', admin.site.urls),
 ]
